66ed441b85846943f91917691d1abc024d7726f3
Kila369 <67660152+Kila369@users.noreply.github.com>
2023-01-11 19:34:54 +0100

    feat(prettier && eslint): setting it up

--- App.js
+++ App.js
@@ -1,25 +1,37 @@
-import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View,Platform,StatusBar } from 'react-native';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import React from "react";
+import { SafeAreaView, StyleSheet, Text, View, StatusBar } from "react-native";
 
 export default function App() {
-  console.log()
+  console.log();
   return (
     <>
-    <SafeAreaView style={{flex: 1, backgroundColor:'blue'}}>
-    <View style={{ flex:1, justifyContent: 'center', paddingTop: StatusBar.currentHeight }}>
-      <View style={{padding:16, backgroundColor:"green"}}>
-        <Text>search</Text>
-      </View>
-      <View style={{flex :1,padding:16, backgroundColor:"blue"}}>
-        <Text>list</Text>
-      </View>
-    </View>
-    </SafeAreaView>
-    <ExpoStatusBar style="auto" />
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
+          <Text>search</Text>
+        </View>
+        <View style={styles.list}>
+          <Text>list</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
     </>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  container: {
+    flex: 1,
+    backgroundColor: "blue",
+    marginTop: StatusBar.currentHeight,
+  },
+  search: {
+    padding: 16,
+    backgroundColor: "green",
+  },
+  list: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "blue",
+  },
 });
