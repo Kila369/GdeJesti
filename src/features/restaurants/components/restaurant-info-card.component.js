bad550f0fc9e0815346449b06155030277c61dba
Kila369 <67660152+Kila369@users.noreply.github.com>
2023-01-14 20:22:53 +0100

    feat(theme): setting up theme

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -3,17 +3,17 @@ import styled from "styled-components";
 import { Card } from "react-native-paper";
 
 const Title = styled.Text`
-  padding: 16px;
-  color: green;
+  padding: ${(props) => props.theme.space[3]};
+  color: ${(props) => props.theme.colors.ui.success};
 `;
 
 const RestaurantCard = styled(Card)`
-  background-color: white;
+  background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 
 const RestaurantCardCover = styled(Card.Cover)`
-  background-color: white;
-  padding: 16px;
+  background-color: ${(props) => props.theme.colors.bg.primary};
+  padding: ${(props) => props.theme.space[3]};
 `;
 
 export const RestaurantInfoCard = ({ restaurant }) => {
--- src/features/screens/restaurants.screen.js
+++ src/features/screens/restaurants.screen.js
@@ -10,11 +10,11 @@ const SafeArea = styled(SafeAreaView)`
 `;
 
 const SearchContainer = styled.View`
-  padding: 16px;
+  padding: ${(props) => props.theme.space[3]};
 `;
 
 const RestaurantListContainer = styled.View`
-  padding: 16px;
+  padding: ${(props) => props.theme.space[3]};
   flex: 1;
 `;
 
